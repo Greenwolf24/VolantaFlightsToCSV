e6261fac9f9479859797d91d34349421a86c0844
greenwolf24 <greenwolf1234@outlook.com>
2022-05-13 17:39:20 -0400

    Change main method to check an entire folder for json files. Added a likelyBuggy check to weed out likely buggy flights. I chose any flight with under 10 positions. This should weed out any canceled flights as well

--- src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToCSV.java
+++ src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToCSV.java
@@ -11,33 +11,47 @@ public class VolantaToCSV
 {
 	public static void main(String[] args)
 	{
-		try
+		File folder = new File("data/JSON-in/");
+		File[] files = folder.listFiles();
+		for(File file : files)
 		{
-			File file = new File("data/JSON-in/2dc71ed9-f8ec-4d9e-870b-a9bcbc8bec3c.json");
-			Flight flight = new Gson().fromJson(new FileReader(file), Flight.class);
-			System.out.println(flight);
-			
-			// basic parse positions into csv
-			String csvOutStr = "Time,Latitude,Longitude,Altitude" + "\n";
-			for (int i = 0; i < flight.Positions.size(); i++)
+			try
 			{
-				String time = flight.Positions.get(i).Time;
-				// we need to reformat the time
-				// the input is in the format "2019-01-01T00:00:00Z"
-				// the output is "2019-01-01 00:00:00"
-				time = time.substring(0, 10) + " " + time.substring(11, 19);
-				csvOutStr += time + "," + flight.Positions.get(i).Latitude + ", " + flight.Positions.get(i).Longitude + "," + flight.Positions.get(i).Altitude + "\n";
+				//File file = new File("data/JSON-in/2dc71ed9-f8ec-4d9e-870b-a9bcbc8bec3c.json");
+				Flight flight = new Gson().fromJson(new FileReader(file), Flight.class);
+				//System.out.println(flight);
+				
+				// This small section is a bit of a hacky way of debugging something I found.
+				// the error appears that there is a piece of flight data directly between two airports for no reason.
+				// this is simple going to print out the filename of the flight if either the origin or destination matches either buggy airport.
+				/*
+				if(flight.OriginIcao.equals("KBRO") || flight.DestinationIcao.equals("MMMD"))
+				{
+					System.out.println(file.getName());
+				}
+				//*/
+				
+				// basic parse positions into csv
+				String csvOutStr = "Time,Latitude,Longitude,Altitude" + "\n";
+				for(int i = 0; i < flight.Positions.size(); i++)
+				{
+					String time = flight.Positions.get(i).Time;
+					// we need to reformat the time
+					// the input is in the format "2019-01-01T00:00:00Z"
+					// the output is "2019-01-01 00:00:00"
+					time = time.substring(0, 10) + " " + time.substring(11, 19);
+					csvOutStr += time + "," + flight.Positions.get(i).Latitude + ", " + flight.Positions.get(i).Longitude + "," + flight.Positions.get(i).Altitude + "\n";
+				}
+				
+				// write to file
+				File csvOutFile = new File("data/CSV-out/" + file.getName().replace(".json", ".csv"));
+				FileWriter csvOut = new FileWriter(csvOutFile);
+				csvOut.write(csvOutStr);
+				csvOut.close();
+			} catch(Exception e)
+			{
+				e.printStackTrace();
 			}
-			
-			// write to file
-			File csvOutFile = new File("data/CSV-out/2dc71ed9-f8ec-4d9e-870b-a9bcbc8bec3c.csv");
-			FileWriter csvOut = new FileWriter(csvOutFile);
-			csvOut.write(csvOutStr);
-			csvOut.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
 		}
 	}
 }
--- src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToKML.java
+++ src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToKML.java
@@ -7,33 +7,75 @@ import io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Flight;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class VolantaToKML
 {
 	public static void main(String[] args)
 	{
-		try
+		File folder = new File("data/JSON-in/");
+		File[] files = folder.listFiles();
+		for(File file : files)
 		{
-			File file = new File("data/JSON-in/2dc71ed9-f8ec-4d9e-870b-a9bcbc8bec3c.json");
-			Flight flight = new Gson().fromJson(new FileReader(file), Flight.class);
-			
-			String filePath = "data/KML-out/";
-			String fileName = "2dc71ed9-f8ec-4d9e-870b-a9bcbc8bec3c";
-			
-			ArrayList<io.github.greenwolf24.KMLmanage.Util.Position> positions = new ArrayList<>();
-			for(io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Position position : flight.Positions)
+			/*
+			if(hardCodeBuggyFlight(file.getName()))
 			{
-				double altitude = position.Altitude;
-				// convert from feet to meters
-				altitude *= 0.3048;
-				positions.add(new io.github.greenwolf24.KMLmanage.Util.Position(position.Latitude, position.Longitude, (int)altitude));
+				continue;
 			}
-			io.github.greenwolf24.KMLmanage.Maker.PathMaker pathMaker = new io.github.greenwolf24.KMLmanage.Maker.PathMaker(filePath);
-			pathMaker.makePathLine( positions, fileName);
+			//*/
+			
+			try
+			{
+				Flight flight = new Gson().fromJson(new FileReader(file), Flight.class);
+				
+				if(likelyBuggyFlight(flight))
+				{
+					System.out.println("Skipping likely buggy file " + file.getName());
+					continue;
+				}
+				
+				String filePath = "data/KML-out/";
+				// we need the filename without the leading path
+				// we will also replace the extension with nothing
+				String fileName = file.getName().replace(".json", "");
+				
+				ArrayList<io.github.greenwolf24.KMLmanage.Util.Position> positions = new ArrayList<>();
+				for(io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Position position : flight.Positions)
+				{
+					double altitude = position.Altitude;
+					// convert from feet to meters
+					altitude *= 0.3048;
+					positions.add(new io.github.greenwolf24.KMLmanage.Util.Position(position.Latitude, position.Longitude, (int) altitude));
+				}
+				io.github.greenwolf24.KMLmanage.Maker.PathMaker pathMaker = new io.github.greenwolf24.KMLmanage.Maker.PathMaker(filePath);
+				pathMaker.makePathLine(positions, fileName);
+			} catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch(Exception e)
+	}
+	
+	private static boolean likelyBuggyFlight(Flight flight)
+	{
+		// if the flight has less than 10 positions, it is probably a buggy flight
+		if(flight.Positions.size() < 10)
 		{
-			e.printStackTrace();
+			return true;
 		}
+		return false;
+	}
+	
+	private static boolean hardCodeBuggyFlight(String fileName)
+	{
+		String[] buggy = {"fcc8504c-3cbf-48d8-b1db-ae5b81827d3c"};
+		for(String s : buggy)
+		{
+			if(s.equals(fileName.replace(".json", "")))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 }
