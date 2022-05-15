8f68dcc49aedac50f3980cb5c2e5102e99db1f12
Greenwolf24 <greenwolf1234@outlook.com>
2022-05-15 05:02:35 -0400

    Updated to use altitude data to colorize flight paths much like FlightRadar24. There is a boolean included in the call on line 45, where the colors can be like the FlightRadar24 downloaded KMLs, or like the web colorings. Notes: There is a bug that I have found that where mid-air may be colored white. I am working on finding the cause. This commit includes the SwissArmyKnife-1.1.0-preRelease.jar, it should be functionally equivalent to the full 1.1.0

--- src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToKML.java
+++ src/io/github/greenwolf24/VolantaFlightsToCSV/VolantaToKML.java
@@ -1,11 +1,14 @@
 package io.github.greenwolf24.VolantaFlightsToCSV;
 
 import com.google.gson.Gson;
+import io.github.greenwolf24.SwissArmyKnife.Color.FlightRadar24ColorRange;
 import io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Flight;
+import io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Position;
 
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -30,7 +33,7 @@ public class VolantaToKML
 				
 				if(likelyBuggyFlight(flight))
 				{
-					System.out.println("Skipping likely buggy file " + file.getName());
+					System.out.println("Skipping likely buggy file " + file.getName() + ". This is flight " + flight.Callsign + "/" + flight.FlightNumber);
 					continue;
 				}
 				
@@ -39,6 +42,17 @@ public class VolantaToKML
 				// we will also replace the extension with nothing
 				String fileName = file.getName().replace(".json", "");
 				
+				String kml = colorLinePath(flight.Positions,flight.Callsign + "/" + flight.FlightNumber,false);
+				
+				File kmlFile = new File(filePath + fileName + ".kml");
+				FileWriter fw = new FileWriter(kmlFile);
+				fw.write(kml);
+				fw.close();
+				
+				
+				
+				/* // This is the old way of doing it, with one single line as the path
+				// I am keeping it here for reference and in case I need/want to revert
 				ArrayList<io.github.greenwolf24.KMLmanage.Util.Position> positions = new ArrayList<>();
 				for(io.github.greenwolf24.VolantaFlightsToCSV.VolantaData.Position position : flight.Positions)
 				{
@@ -49,6 +63,8 @@ public class VolantaToKML
 				}
 				io.github.greenwolf24.KMLmanage.Maker.PathMaker pathMaker = new io.github.greenwolf24.KMLmanage.Maker.PathMaker(filePath);
 				pathMaker.makePathLine(positions, fileName);
+				//*/
+				
 			} catch(Exception e)
 			{
 				e.printStackTrace();
@@ -77,5 +93,86 @@ public class VolantaToKML
 			}
 		}
 		return false;
+	}
+	
+	private static String onePlacemark(Position position1, Position position2,String name,boolean flightRadarWebFormat)
+	{
+		// TODO: impliment this
+		
+		String base = "<Placemark>\n" +
+				"        <Style>\n" +
+				"          <LineStyle>\n" +
+				"            <color>COLOR</color>\n" + // AA RR GG BB, no #
+				"            <width>2</width>\n" +
+				"          </LineStyle>\n" +
+				"        </Style>\n" +
+				"        <MultiGeometry>\n" +
+				"          <LineString>\n" +
+				"            <tessellate>1</tessellate>\n" +
+				"            <altitudeMode>MODE</altitudeMode>\n" + // orig: absolute
+				"            <coordinates>COORDS</coordinates>\n" + // orig: -80.269379,25.913589,38.1 -80.263947,25.913862,121.92    Lat,Lon,AltMeters
+				"          </LineString>\n" +
+				"        </MultiGeometry>\n" +
+				"        <name>NAME</name>\n" + // Orig: P-50
+				"      </Placemark>";
+		
+		if(position1.OnGround && position2.OnGround)
+		{
+			base = base.replace("MODE","clampToGround");
+			base = base.replace("COLOR","ffffffff");
+			// because we are on the ground, we can fill the altitude as 0
+			base = base.replace("COORDS",position1.Longitude + "," + position1.Latitude + ",0 " + position2.Longitude + "," + position2.Latitude + ",0");
+			base = base.replace("NAME",name);
+		}
+		else
+		{
+			base = base.replace("MODE","absolute");
+			if(flightRadarWebFormat)
+			{
+				base = base.replace("COLOR", new FlightRadar24ColorRange().getColor((int) position1.Altitude).kmlFormat());
+			}
+			else
+			{
+				base = base.replace("COLOR", new FlightRadar24ColorRange().getColor((int) position1.Altitude).rawString(true));
+			}
+			// the Positions are in feet, convert to meters
+			base = base.replace("COORDS",position1.Longitude + "," + position1.Latitude + "," + (position1.Altitude * 0.3048) + " " + position2.Longitude + "," + position2.Latitude + "," + (position2.Altitude * 0.3048));
+			base = base.replace("NAME",name);
+		}
+		
+		return base;
+	}
+	
+	private static String colorLinePath(ArrayList<Position> positions, String name, boolean flightRadarWebFormat)
+	{
+		String top0 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n" +
+				"  <Document>\n" +
+				"    <name>NAME</name>";
+		
+		String top1 = "<Folder>\n" +
+				"      <name>Trail</name>";
+		
+		String bottom = " </Folder>\n" +
+				"  </Document>\n" +
+				"</kml>";
+		
+		
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(top0.replace("NAME",name));
+		sb.append(top1);
+		
+		for(int i = 0; i < positions.size() - 1; i++)
+		{
+			Position position1 = positions.get(i);
+			Position position2 = positions.get(i + 1);
+			
+			sb.append(onePlacemark(position1, position2, "P-" + i,flightRadarWebFormat));
+		}
+		
+		sb.append(bottom);
+		
+		return sb.toString();
 	}
 }
